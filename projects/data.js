dcad635728d940cfcfd53e32cd86c64b6c774dc2
tanbopp <sltnsvenn@gmail.com>
2025-06-26 19:33:04 +0700

    Replace portfolio with new version

---
+++ projects/data.js
@@ -0,0 +1,28 @@
+// projects/data.js
+
+export const applications = [
+  {
+    title: "E-commerce Platform",
+    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    tags: ["UI Design", "Frontend", "E-commerce"],
+    link: "#work"
+  },
+  {
+    title: "E-commerce Platform",
+    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    tags: ["UI Design", "Frontend", "E-commerce"],
+    link: "#work"
+  },
+  {
+    title: "E-commerce Platform",
+    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    tags: ["UI Design", "Frontend", "E-commerce"],
+    link: "#work"
+  },
+  {
+    title: "E-commerce Platform",
+    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    tags: ["UI Design", "Frontend", "E-commerce"],
+    link: "#work"
+  }
+];
